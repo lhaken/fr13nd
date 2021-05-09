1708e3faa9a5b8a689ff5e0e25e0d7fd711bd81e
lhaken <56554206+lhaken@users.noreply.github.com>
2021-05-09 15:44:01 +0200

    Add files via upload

--- fr13nd.py
+++ fr13nd.py
@@ -1,3 +1,3 @@
 # fr13nd.py
-print("Hello from your friend Lordy")
-input("Press Enter to continue...")
+print("Hello from your friend! -Lordy")
+input("Press Enter to continue...") # this line makes the pop up window not close after printing
